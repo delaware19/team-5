959c13c9fe481f2aaaf9f74671c9c5ee2d24bea4
Ryan Cohen <jetsonrwc213@gmail.com>
2019-09-27 20:25:07 -0400

    Added home.html and layout.html in templates directory

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+
+    return render_template("home.html", title = "Home Page")
+
+
+
 
 if __name__ == "__main__":
     app.run()
