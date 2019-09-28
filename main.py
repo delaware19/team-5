731da56f32f83e965e7d4c24395e1568db145189
Ryan Emenheiser <ryane@udel.edu>
2019-09-27 21:47:25 -0400

    Updated database

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+<<<<<<< HEAD
+import sqlite3
+from flask import Flask, render_template
+from flask import g
+
+DATABASE = '/Users/ryan_emenheiser/Desktop/CodeForGood/team-5/c4gDataBase.db'
+app = Flask(__name__)
+
+
+@app.route("/home")
+def home():
+
+    return render_template("home.html", title = "Home Page")
+
+
+#Gain access to db
+def get_database():
+    database = getattr(g, '_database', None)
+    if database is None:
+        database = g._database = sqlite3.connect(DATABASE)
+
+
+@app.teardown_appcontext
+def close_database(*args, **kwargs):
+    database = getattr(g, '_database', None)
+    if database is not None:
+        database.close()
+
+
+if __name__ == "__main__":
+    app.run()
+=======
 import sqlite3
 from flask import Flask, render_template
 
@@ -36,3 +68,4 @@ def close_database(exception):
 
 if __name__ == "__main__":
     app.run()
+>>>>>>> 4ecaca27b2bb4b1421348ffdb49fdbaa8225308d
