26f5293153528c90de4d444eb05cc38989532ded
root <root@DESKTOP-OQDM2N0.localdomain>
2019-09-27 20:55:41 -0400

    db connection skeleton code

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import sqlite3
 from flask import Flask
+
+DATABASE = '/path/to/database.db'
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+#Gain access to db
+def get_database():
+    database = getattr(Flask, '_database', None)
+    if database is None:
+        database = Flask._database = sqlite3.connect(DATABASE)
+
+
+@app.teardown_appcontext
+def close_database():
+    database = getattr(Flask, '_database', None)
+    if database is not None:
+        database.close()
+
 
 if __name__ == "__main__":
     app.run()
