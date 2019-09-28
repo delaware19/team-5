1019ce625fe11099cf3850e25ddb0a27547c1fa7
root <root@DESKTOP-OQDM2N0.localdomain>
2019-09-27 21:54:07 -0400

    endpoint bug fix

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sqlite3
 from flask import Flask, render_template
 from flask import g
@@ -7,31 +6,9 @@ DATABASE = '/Users/ryan_emenheiser/Desktop/CodeForGood/team-5/c4gDataBase.db'
 app = Flask(__name__)
 
 
-@app.route("/home")
+@app.route("/")
 def home():
-
     return render_template("home.html", title = "Home Page")
-
-
-#Gain access to db
-def get_database():
-    database = getattr(g, '_database', None)
-    if database is None:
-        database = g._database = sqlite3.connect(DATABASE)
-
-
-@app.teardown_appcontext
-def close_database(*args, **kwargs):
-    database = getattr(g, '_database', None)
-    if database is not None:
-        database.close()
-
-
-if __name__ == "__main__":
-    app.run()
-=======
-import sqlite3
-from flask import Flask, render_template
 
 DATABASE = '/Users/ryan_emenheiser/Desktop/CodeForGood/team-5/c4gDataBase.db'
 app = Flask(__name__)
@@ -68,4 +45,4 @@ def close_database(exception):
 
 if __name__ == "__main__":
     app.run()
->>>>>>> 4ecaca27b2bb4b1421348ffdb49fdbaa8225308d
+
